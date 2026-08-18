49033e0a645bb431f4fe0e7e8a0352c6dbf52fb9
Vũ Trương Huy Hoàng <vthh13042006@gmail.com>
2026-08-18 18:21:53 +0700

    Create dummy_data.py

---
+++ dummy_data.py
@@ -0,0 +1,6 @@
+danh_sach_test = [
+    {"PID": "P1", "AT": 0, "BT": 5, "PR": 2},
+    {"PID": "P2", "AT": 1, "BT": 3, "PR": 1},
+    {"PID": "P3", "AT": 2, "BT": 8, "PR": 3},
+    {"PID": "P4", "AT": 4, "BT": 2, "PR": 4}
+]
